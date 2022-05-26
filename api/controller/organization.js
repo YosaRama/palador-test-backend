65dc1e608eef6e9f4dbbbb9734cf0c9a310c08d1
Yosa Rama <yosamelody07@gmail.com>
2022-05-27 02:08:12 +0800

    ADD: Complete organization API controller and route

--- api/controller/organization.js
+++ api/controller/organization.js
@@ -5,6 +5,49 @@ const getAllEmployee = async () => {
   return employee;
 };
 
+const getSingleEmployee = async (id) => {
+  const employee = await prisma.organization.findUnique({
+    where: { employee_id: id },
+  });
+  return employee;
+};
+
+const createEmployee = async (data) => {
+  const { name, managerId } = data;
+  const latestId = await prisma.organization.findFirst({
+    select: { employee_id: true },
+    orderBy: { id: "desc" },
+  });
+  await prisma.organization.create({
+    data: {
+      employee_id: +latestId.employee_id + 1,
+      name: name,
+      manager_id: managerId ? managerId : null,
+    },
+  });
+};
+
+const updateEmployee = async (id, data) => {
+  const { name, manager_id } = data;
+  await prisma.organization.update({
+    where: { employee_id: id },
+    data: {
+      name: name,
+      manager_id: +manager_id,
+    },
+  });
+};
+
+const deleteEmployee = async (id) => {
+  await prisma.organization.delete({
+    where: { employee_id: id },
+  });
+};
+
 module.exports = {
   getAllEmployee: getAllEmployee,
+  getSingleEmployee: getSingleEmployee,
+  createEmployee: createEmployee,
+  updateEmployee: updateEmployee,
+  deleteEmployee: deleteEmployee,
 };
--- api/router/organization.js
+++ api/router/organization.js
@@ -1,17 +1,113 @@
 //? Libs
 const express = require("express");
+const bodyParser = require("body-parser");
 const router = express.Router();
+router.use(bodyParser.urlencoded({ extended: false }));
+router.use(bodyParser.json());
 
 //? Controller
-const { getAllEmployee } = require("../controller/organization");
+const {
+  getAllEmployee,
+  getSingleEmployee,
+  createEmployee,
+  deleteEmployee,
+  updateEmployee,
+} = require("../controller/organization");
 
+//? Get all employee
 router.get("/", async (req, res) => {
-  const allEmployee = await getAllEmployee();
-  res.status(200).json({
-    success: true,
-    data: allEmployee,
-    message: "This is Organization",
-  });
+  try {
+    const allEmployee = await getAllEmployee();
+    res.status(200).json({
+      success: true,
+      message: "Successfully get all employee",
+      data: allEmployee,
+    });
+  } catch (error) {
+    res.status(200).json({
+      success: false,
+      message: "Failed to get all employee",
+      error: error.message,
+    });
+  }
+});
+
+//? Get single employee
+router.get("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const singleEmployee = await getSingleEmployee(+id);
+    res.status(200).json({
+      success: true,
+      message: "Successfully get single employee",
+      data: singleEmployee,
+    });
+  } catch (error) {
+    res.status(200).json({
+      success: false,
+      message: "Failed to get single employee",
+      error: error.message,
+    });
+  }
+});
+
+//? Create employee
+router.post("/", async (req, res) => {
+  const data = req.body;
+  try {
+    const createdEmployee = await createEmployee(data);
+    res.status(200).json({
+      success: true,
+      message: "Successfully create employee",
+      data: createdEmployee,
+    });
+  } catch (error) {
+    res.status(200).json({
+      success: false,
+      message: "Failed to create employee",
+      error: error.message,
+    });
+  }
+});
+
+//? Update employee
+router.put("/:id", async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+  try {
+    const editedEmployee = await updateEmployee(+id, data);
+    res.status(200).json({
+      success: true,
+      message: "Successfully update employee",
+      data: editedEmployee,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(200).json({
+      success: false,
+      message: "Failed to update employee",
+      error: error.message,
+    });
+  }
+});
+
+//? Delete employee
+router.delete("/:id", async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deletedEmployee = await deleteEmployee(+id);
+    res.status(200).json({
+      success: true,
+      message: "Successfully delete employee",
+      data: deletedEmployee,
+    });
+  } catch (error) {
+    res.status(200).json({
+      success: false,
+      message: "Failed to delete employee",
+      error: error.message,
+    });
+  }
 });
 
 module.exports = router;
